6b15fe4938284176cdb508927ec956d6600c4cdb
A.N <77659600+Barking-Owl@users.noreply.github.com>
2022-01-24 15:09:46 -0600

    Cube Setup
    Added initial skeleton of C# code for Random Cubes and added Cube prefab

---
+++ RandomCubes_Unity/Assets/Scripts/RandomCubes.cs
@@ -0,0 +1,27 @@
+/****
+ * Created by: Andrew Nguyen
+ * Created on 1/24/2022
+ * Last Edited by: Andrew Nguyen
+ * Last Edited on: 1/24/2022
+ * 
+ * Description: Spawns multiple cube prefabs into the scene
+ ****/
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RandomCubes : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
